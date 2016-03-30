b925706c3f9169d3894630c3524881b2c2d1f3e1
madebyeggs <madebyeggs@gmail.com>
2016-03-30 10:47:24 +0100

    fb share

--- app/assets/javascripts/FB_SDK.js
+++ app/assets/javascripts/FB_SDK.js
@@ -1,15 +1,61 @@
-window.fbAsyncInit = function() {
-	FB.init({
-    	appId      : '838327899644745',
-     	xfbml      : true,
-     	version    : 'v2.5'
-   	});
+/* by Steven Yang, Feb 2015, originally for www.mathscore.com.  This code is free for anybody to use as long as you include this comment.  */
+function FacebookFeedDialog(appID, linkTarget, redirectTarget) {
+  this.mParams = {
+    app_id: appID,
+    link: linkTarget,
+    redirect_uri: redirectTarget,
+    display: "popup"
+  }
 };
 
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-   	if (d.getElementById(id)) {return;}
-   	js = d.createElement(s); js.id = id;
-   	js.src = "//connect.facebook.net/en_US/sdk.js";
-   	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+/* Common params include:
+   name - the title that appears in bold font
+   description - the text that appears below the title
+   picture - complete URL path to the image on the left of the dialog
+   caption - replaces the link text
+*/
+FacebookFeedDialog.prototype.addParam = function(key, value) {
+  this.mParams[key] = value;
+};
+
+FacebookFeedDialog.prototype.open = function() {
+
+  var url = 'https://www.facebook.com/dialog/feed?' + encodeCGIArgs(this.mParams);
+  popup(url, 'feedDialog', 700, 400);
+};
+
+/* Takes a param object like this:
+   { arg1: "value1", arg2: "value2" }
+   and converts into CGI args like this:
+   arg1=value1&arg2=value2
+
+   The values and args will be properly URI encoded
+*/
+function encodeCGIArgs(paramObject) {
+
+  var result = '';
+
+  for (var key in paramObject) {
+    if (result)
+      result += '&';
+    result += encodeURIComponent(key) + '=' + encodeURIComponent(paramObject[key]);
+  }
+
+  return result;
+}
+
+function popup(mylink,windowname,width,height) {
+  if (!window.focus) return;
+  var href;
+  if (typeof(mylink) == 'string')
+    href=mylink;
+  else
+    href=mylink.href;
+  if (!windowname)
+    windowname='mywindow';
+  if (!width)
+    width=600;
+  if (!height)
+    height=350;
+  window.open(href, windowname, 'resizable=yes,width='+width+',height='+height+',scrollbars=yes');
+}
