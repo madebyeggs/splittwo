38a1a4c0ccfb00cf13484afaf728a81fe3b28777
madebyeggs <madebyeggs@gmail.com>
2016-03-31 22:08:23 +0100

    rescue nil and slides controller show meta tags added

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -17,6 +17,20 @@ class SlidesController < ApplicationController
   
   def show
     @slide = Announcement.find_by_slug(params[:id]) || Artist.find_by_slug(params[:id]) || Work.find_by_slug(params[:id]) || Release.find_by_slug(params[:id])
+    set_meta_tags og: {
+      url: "#{@currentUrl}",
+      image: "#{@slide.fb_image}",
+      title: "#{@slide.slide_title}",
+      description: "#{@slide.description}",
+      type: "musician"
+    }
+    set_meta_tags twitter: {
+      card: "summary_large_image",
+      site: "@Fairsplitmusic",
+      title: "#{@slide.slide_title}",
+      description: "#{@slide.description}",
+      image: "#{@slide.fb_image}"
+    }
   end
   
 end
