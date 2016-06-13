279310380061fb9efe33b8e20d1233a4d8397cfa
madebyeggs <madebyeggs@gmail.com>
2016-06-13 14:29:43 +0100

    slides ordered by date created at

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,10 @@ class ApplicationController < ActionController::Base
     @slide_artists = Artist.where('slideshow = ?', true)
     @slide_works = Work.where('slideshow = ?', true)
     @slide_releases = Release.where('slideshow = ?', true)
-    @slides = @slide_announcements + @slide_artists + @slide_works + @slide_releases
+    @slides = @slide_announcements + @slide_artists + @slide_works + @slide_releases.order('created_at ASC')
+    @slides_again= [@slide_announcements, @slide_artists, @slide_works, @slide_releases].flatten
+    @sorted_slides = @slides_again.sort_by { |item| item.created_at.strftime('%m/%d/%y') }
+    #@slides = Announcement.where("slideshow = ?", true) & Artist.where("slideshow = ?", true) & Work.where("slideshow = ?", true) & Release.where("slideshow = ?", true)
   end
   
 end
